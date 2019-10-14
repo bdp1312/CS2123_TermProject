7b9cb13123ca11a908e5556292935f85b69c4b93
Benjamin Parrish <bdp1312@gmail.com>
2019-10-14 13:49:33 -0500

    Initial commit

---
+++ Driver0.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+
+
+int main(){
+	int mode, input;
+	scanf("%d",&input);
+	if(input==1){mode+=6;}
+	input=0;
+	while(input>6 || input<1){
+		scanf("%d",&input);
+	}
+	mode+=input;
+	if(mode==1){}
+	else if(mode==2){}
+	else if(mode==3){}
+	else if(mode==4){}
+	else if(mode==5){}
+	else if(mode==6){}
+	else if(mode==7){}
+	else if(mode==8){}
+	else if(mode==9){}
+	else if(mode==10){}
+	else if(mode==11){}
+	else if(mode==12){}
+	else{}
+
+}
+
+
+
