3a25e11d9517f6ab410e30384cb1f9128f7cebd0
Benjamin Parrish <bdp1312@gmail.com>
2019-10-15 15:54:47 -0500

    wrote lib funtions for int data type

--- ArrayLib.c
+++ ArrayLib.c
@@ -2,8 +2,60 @@
 
 #include"ArrayLib.h"
 
-
-
-int insertElement(int ** myArray, int targetIndex, int arrayLen, int newElement){
+//returns 1 for true, 0 for false
+int isValidIndex(int targetIndex, int workingLength){
+  //helper funtion, checks if targetIndex is less than relevent array length
+  if(workingLength > targetIndex){return 1;}
   return 0;
+}
+//isValidIndex() curantly unused
+
+int insertIntElement(int ** myArray, int targetIndex, int numElements, int newElement){
+  // if(isValidIndex(targetIndex, numElements)==0){
+  //   return 0;
+  // }else{
+    *myArray[numElements] = newElement;
+    int i;
+    for(i=numElements;i>targetIndex; --i){
+      swapIntElement(myArray, i, i-1);
+    }
+  // }
+  return 1;
+}
+
+
+int removeIntElement(int **myArray, int targetIndex, int numElements){
+  int i;
+    for(i=numElements-1; i>targetIndex;--i){
+      swapIntElement(myArray, i, i-1);
+    }
+  return 1;
+}
+
+
+int swapIntElement(int **myArray, int firstIndex, int secondIndex){
+  // if(isValidIndex(firstIndex, numElements)==0 || isValidIndex(secondIndex, numElements) == 0){
+  //   return 0; //returns failure if array length isn't greather than both indexes
+  // } else {    //else swap the values
+    int tempVal = *myArray[firstIndex];
+    *myArray[firstIndex] = *myArray[secondIndex];
+    *myArray[secondIndex] = tempVal;
+//  }
+  return 1;
+}
+
+
+
+int insertStringElement(char ***myArray, int targetIndex, int numElements, char *newElement){
+  return 1;
+}
+
+
+int removeStringElement(char ***myArray, int targetIndex, int numElements){
+  return 1;
+}
+
+
+int swapStringElement(char ***myArray, int firstIndex, int secondIndex){
+  return 1;
 }
--- ArrayLib.h
+++ ArrayLib.h
@@ -1,8 +1,45 @@
 #ifndef ARRAYLIB_H
 #define ARRAYLIB_H
-//returns 1 if successfull, if targetIndex >= arrayLen return 0
-int insertIntElement(int **myArray, int targetIndex, int arrayLen, int newElement);
-//inserts newElement at targetIndex of myArray if targetIndex<arrayLen
+
+//functions that are to be used multiple times in diferant algorithms
+
+/*INTEGER FUNTIONS*/
+
+  //returns 1 if successfull, if fails return 0
+  int insertIntElement(int **myArray, int targetIndex, int numElements, int newElement);
+  //inserts newElement at targetIndex of myArray, increments positions of all element at higher indexes
+  //numElements is the number of elements in the array initially
+
+  //returns 1 if successfull, 0 if failure.
+  int removeIntElement(int **myArray, int targetIndex, int numElements);
+  //removes element at targetIndex of myArray, deincrements positions of all ele at higher indexes
+  //funtion will live a duplicate of the last element in the array at &myArray[numElements]
+  //numElements is the number of elements in the array initially
+
+  //returns 1 if successfull, 0 if failure
+  int swapIntElement(int **myArray, int firstIndex, int secondIndex);
+  //switches the elements at firstIndex and secondIndex in myArray
+
+/*STRING FUNCTIONS*/
+
+  //returns 1 if successfull, 0 if failure.
+  int insertStringElement(char ***myArray, int targetIndex, int numElements, char *newElement);
+  //inserts newElement at targetIndex of myArray, increments positions of all element at higher indexes
+  //numElements is the number of elements in the array initially
+
+  //returns 1 if successfull, 0 if failure.
+  int removeStringElement(char ***myArray, int targetIndex, int numElements);
+  //removes element at targetIndex of myArray if targetIndex < numElements
+  //funtion will live a duplicate of the last element in the array at &myArray[numElements]
+  //numElements is the number of elements in the array initially
+
+  //returns 1 if successfull, 0 if failure
+  int swapStringElement(char ***myArray, int firstIndex, int secondIndex);
+  //switches the elements at firstIndex and secondIndex in myArray
+
+
+
+
 
 
 #endif
