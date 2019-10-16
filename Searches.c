a7fc09247bebe41452fd196efc4d335c96db0a21
Benjamin Parrish <bdp1312@gmail.com>
2019-10-15 22:52:35 -0500

    forgot to save a file

--- Searches.c
+++ Searches.c
@@ -1,6 +1,8 @@
+#include<string.h>
+
 #include "ArrayLib.h"
 
-
+//returns index of found element or '-1' if fails
 int linIntArraySearch(**int myArray, int sizeOfArray, int soughtElement){
   int i;
   for(i = 0; i<sizeOfArray; ++i){
@@ -11,11 +13,13 @@ int linIntArraySearch(**int myArray, int sizeOfArray, int soughtElement){
   return -1;
 }
 
-
+//returns index of found element or '-1' if fails
 int linStringAraySearch(***char myArray, int sizeOfArray, char *soughtElement);
 
+//returns index of found element or '-1' if fails
+int biIntArraySearch(**int myArray, int sizeOfArray, int soughtElement){
+  
+}
 
-int biIntArraySearch(**int myArray, int sizeOfArray, int soughtElement);
-
-
+//returns index of found element or '-1' if fails
 int biStringArraySearch(***char myArray, int sizeOfArray, char *soughtElement);
